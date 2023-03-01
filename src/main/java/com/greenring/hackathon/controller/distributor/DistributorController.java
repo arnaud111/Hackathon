105cdd10d173715e1c26373c6d5ee37e4b091094
Arnaud <arnaudjourdain@orange.fr>
2023-03-01 19:58:39 +0100

    Initial commit

---
+++ src/main/java/com/greenring/hackathon/controller/distributor/DistributorController.java
@@ -0,0 +1,9 @@
+package com.greenring.hackathon.controller.distributor;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/distributor")
+public class DistributorController {
+}
---
+++ src/main/java/com/greenring/hackathon/controller/trash/TrashController.java
@@ -0,0 +1,9 @@
+package com.greenring.hackathon.controller.trash;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/trash")
+public class TrashController {
+}
